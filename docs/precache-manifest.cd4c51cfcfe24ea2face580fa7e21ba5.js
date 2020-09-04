51bc8769225ba06354b03d628ace5b9c41617479
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-04 22:24:08 +0200

    building PWA

---
+++ docs/precache-manifest.cd4c51cfcfe24ea2face580fa7e21ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678a53c6a63f906fccb1f0689656e926",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7ba17bfcb2d42314667",
+    "url": "./static/css/main.96d66659.chunk.css"
+  },
+  {
+    "revision": "403d51e2de277a940a36",
+    "url": "./static/js/2.6d72bf43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6d72bf43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ba17bfcb2d42314667",
+    "url": "./static/js/main.46c8b36c.chunk.js"
+  },
+  {
+    "revision": "250991267eb7dc75e4d8",
+    "url": "./static/js/runtime-main.8962b40e.js"
+  }
+]);
