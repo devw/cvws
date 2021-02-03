e8dd0b845b84bd0e262568c5be7a8f57b4b2b1a6
Antonio Pierro <antonio.pierro@gmail.com>
2021-02-03 14:26:06 +0100

    Update st.js

--- docs/st.js
+++ docs/st.js
@@ -20,7 +20,7 @@ const addJS = (url) => {
     HEAD.appendChild(script);
 };
 
-addJS(`${ENDPOINT}/bundle.js?t=06`);
+addJS(`${ENDPOINT}/bundle.js?t=07`);
 addCSS(`${ENDPOINT}/main.css?v=07`);
 addCSS(
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
